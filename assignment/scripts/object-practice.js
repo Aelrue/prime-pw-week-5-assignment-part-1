f23701b6bc00edceb94ffb87fd9a44e8b252dab2
Alexis Mike <littlehoatzin@gmail.com>
2022-10-29 00:22:23 -0500

    finished #1

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -1,4 +1,4 @@
-console.log('***** Object Practice *****')
+console.log("***** Object Practice *****");
 
 // Add the required code to complete the tasks below
 
@@ -8,26 +8,27 @@ console.log('***** Object Practice *****')
   - Give it  a numeric 'shoeCount' property
   - Also give it an array 'favThreeFoods'
 */
-const me = {
+let me = {
+  firstName: "Allie",
+  lastName: "Schwartzbauer",
+  hasSiblings: true,
+  shoeCount: 8.5,
+  favThreeFoods: ["sushi", "coffee", "fried rice"],
   // TODO - add properties here
 };
-console.log('A little about me:', me);
 
 /* 2. Accessing object properties.
   - Create a variable called fullName 
   - Use the firstName & lastName properties of the object you 
     created above to set its value. 
   - Console.log fullName
-*/
-
-
+*/ let fullName = { firstName, lastName };
+console.log(fullName);
 
 /* 3. Nested arrays
   - Console.log your first favorite food
   - Console.log your last favorite food 
 */
-
-
 
 /* 4. Change a property of an existing object.
   - Console.log your current shoe count.
@@ -35,7 +36,6 @@ console.log('A little about me:', me);
   - Set your object's shoeCount to be what it was plus 1
   - Console.log your updated shoe count. 
 */
-
 
 /* 5. Add a new property to an existing object.
   - Add a 'favoriteColor' to your existing object
